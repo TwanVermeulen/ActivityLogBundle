12b207be908085d3f0bb3a5cfd2848c4e4e7fc3f
Twan Vermeulen <twan.vermeulen@skydreams.nl>
2017-08-25 12:49:05 +0200

    Different approach

--- Entity/LogEntry.php
+++ Entity/LogEntry.php
@@ -19,7 +19,7 @@ use Doctrine\ORM\Mapping as ORM;
  *  }
  * )
  * @ORM\Entity(repositoryClass="ActivityLogBundle\Repository\LogEntryRepository")
- * @ORM\EntityListeners({"LogEntryListener"})
+ * @ORM\HasLifecycleCallbacks()
  */
 class LogEntry extends AbstractLogEntry
 {
